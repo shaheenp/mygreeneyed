f7d5c2a9185265c6119602bb3d9a0cd804dcd9c6
Shaheen Page <shaheenpage@icloud.com>
2020-04-14 13:54:56 -0700

    respect system dark mode preference

--- js/index.js
+++ js/index.js
@@ -7,6 +7,7 @@
         }
 
         init() {
+            this.prefersDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
             this.sunsetElement = document.querySelector('img#sunset');
             this.sunriseElement = document.querySelector('img#sunrise');
             this.socialElements = Array.from(document.querySelectorAll('img.social'));
@@ -14,16 +15,19 @@
             this.sunsetElement.addEventListener('click', () => this.sunset());
             this.sunriseElement.addEventListener('click', () => this.sunrise());
 
-            if (window.localStorage.getItem('night')) {
-                this.sunset();
+            if (this.prefersDarkMode || window.localStorage.getItem('night')) {
+                if (!window.localStorage.getItem('preferenceOverride')) {
+                    this.sunset();
+                }
             }
         }
 
         sunset() {
-            const {sunsetElement, sunriseElement, socialElements} = this;
+            const {sunsetElement, sunriseElement, socialElements, prefersDarkMode} = this;
 
             document.body.classList.add('night');
             window.localStorage.setItem('night', 'true');
+            window.localStorage.removeItem('preferenceOverride');
             sunsetElement.style.display = 'none';
             sunriseElement.style.display = '';
             socialElements.forEach(img => {
@@ -32,10 +36,15 @@
         }
 
         sunrise() {
-            const {sunsetElement, sunriseElement, socialElements} = this;
+            const {sunsetElement, sunriseElement, socialElements, prefersDarkMode} = this;
 
             document.body.classList.remove('night');
             window.localStorage.removeItem('night');
+
+            if (prefersDarkMode) {
+                window.localStorage.setItem('preferenceOverride', 'true');
+            }
+
             sunsetElement.style.display = '';
             sunriseElement.style.display = 'none';
             socialElements.forEach(img => {
